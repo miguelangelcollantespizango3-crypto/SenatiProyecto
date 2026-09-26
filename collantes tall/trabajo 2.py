c7af2c574d65965bea2f968609ade356d1683484
miguelangelcollantespizango3-crypto <miguelangelcollantespizango3@gmail.com>
2026-09-26 17:09:19 -0500

    Add files via upload

---
+++ collantes tall/trabajo 2.py
@@ -0,0 +1,12 @@
+x = 10
+x = "diez"
+print("El dato es: ",x)
+
+edad Juana = "20"
+edad = int(edadjuana)
+precio = str(15.5)
+print("Edad de juana: ", edadjuana)
+print("precio en STRING: ",precio)
+    
+type(True)
+64444444462
